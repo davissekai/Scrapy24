5b72c59f68e12f7198afca6133f7449af95716fd
Davis Dey <davisdelali24@gmail.com>
2024-01-19 13:32:59 -0800

    scraper file

---
+++ scraper.py
@@ -0,0 +1,21 @@
+import scrapy 
+
+class QuoteSpider(scrapy.Spider):
+    name = 'quote-spider'
+    start_urls = ['https://quotes.toscrape.com']
+
+    
+    def parse(self, response):
+        QUOTE_SELECTOR = '.quote'
+        TEXT_SELECTOR = '.text::text'
+        AUTHOR_SELECTOR = '.author::text'
+        ABOUT_SELECTOR = '.author + a::attr("href")'
+        TAG_SELECTOR = '.tags > .tag::text'
+
+        for quote in response.css(QUOTE_SELECTOR):
+            yield{
+                'text': quote.css(TEXT_SELECTOR).extract_first(),
+                'author': quote.css(AUTHOR_SELECTOR).extract_first(),
+                'author': 'https://quotes.toscrape.com' + quote.css(ABOUT_SELECTOR).extract_first(),
+                'tags': quote.css(TAG_SELECTOR).extract_first(),
+            }
